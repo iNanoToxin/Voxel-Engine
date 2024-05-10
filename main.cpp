b7c6ee4cc4ce7762ed12b81bc87f9f384c9b5ec2
iNanoToxin <dylanleodaniel@gmail.com>
2024-05-10 04:09:00 -0400

    First commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+#include "third-party/glm/glm.hpp"
+// #include "glm/glm.hpp"
+
+int main()
+{
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}
